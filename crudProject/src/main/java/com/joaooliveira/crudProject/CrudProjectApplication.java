3347f59ca708a9f75dc55763e95ecf15adb6fc2e
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-10-22 12:20:21 +0100

    feat - added swagger-ui

--- crudProject/src/main/java/com/joaooliveira/crudProject/CrudProjectApplication.java
+++ crudProject/src/main/java/com/joaooliveira/crudProject/CrudProjectApplication.java
@@ -8,6 +8,6 @@ public class CrudProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudProjectApplication.class, args);
+        System.out.println("Open SwaggerUI: http://localhost:8080/swagger-ui/index.html");
 	}
-
 }
